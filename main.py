023ff3d8ede9523f7e711ae04bbdccc09f468d4e
Igboke Daniel <danieligboke669@gmail.com>
2025-04-21 15:41:48 +0100

    Save YouTube video details to an Excel sheet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+import pandas as pd
+import openpyxl
 
 load_dotenv()
 
@@ -30,10 +32,25 @@ response = youtube.playlistItems().list(
 ).execute()
 
 # Print the video titles and IDs
+# for item in response["items"]:
+#     print(item["snippet"]["title"])
+#     print(item["snippet"]["resourceId"]["videoId"])
+#     print(item["snippet"]["thumbnails"]["default"]["url"])
+#     link = f"https://www.youtube.com/watch?v={item["snippet"]["resourceId"]["videoId"]}"
+#     print(link)
+#     print()
+
+#save the details to an excel sheet
+video_data = []
 for item in response["items"]:
-    print(item["snippet"]["title"])
-    print(item["snippet"]["resourceId"]["videoId"])
-    print(item["snippet"]["thumbnails"]["default"]["url"])
-    link = f"https://www.youtube.com/watch?v={item["snippet"]["resourceId"]["videoId"]}"
-    print(link)
-    print()
+    video_data.append({
+        "Title": item["snippet"]["title"],
+        "Video ID": item["snippet"]["resourceId"]["videoId"],
+        "Thumbnail URL": item["snippet"]["thumbnails"]["default"]["url"],
+        "Link": f"https://www.youtube.com/watch?v={item['snippet']['resourceId']['videoId']}"
+    })
+df = pd.DataFrame(video_data)
+excel_file = "youtube_videos.xlsx"
+with pd.ExcelWriter(excel_file, engine='openpyxl', mode='a' if os.path.exists(excel_file) else 'w') as writer:
+    df.to_excel(writer, index=False, sheet_name="Videos")
+print(f"Video details saved to {excel_file}")
